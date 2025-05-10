45e0c4652f30f335335ec20337ac5a1197ad8ab8
Juan Cardona <jcardonabageth@gmail.com>
2025-05-10 09:16:47 -0500

    Corrige la importación de AuthMethods en el archivo de tipos de autenticación

--- src/features/auth/basic/types/index.ts
+++ src/features/auth/basic/types/index.ts
@@ -1,4 +1,4 @@
-import { AuthMethods } from '@/infrastructure/mongoDb/Models/User/Entity';
+import { AuthMethods } from "@/infrastructure/mongoDb/Models/User";
 
 export type LoginRequestData = {
   email: string;
